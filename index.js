dff789ae33bb534d0ac2839d835dc9d0a2c0b2df
fangkay <fangkay28@gmail.com>
2022-07-04 11:08:07 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const { instrument } = require("@socket.io/admin-ui");
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", "https://admin.socket.io/"],
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server is running");
+});
+
+instrument(io, { auth: false });
